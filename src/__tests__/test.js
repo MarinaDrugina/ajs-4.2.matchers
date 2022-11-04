550bce56e14dddc970e7035d8b1645acd4b0e9d5
Marina Drugina <druginamarina@gmail.com>
2022-11-04 18:01:41 +0300

    deleted comments

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,22 +1,3 @@
-/*import sortCharacters from '../js/app.js';
-
-test('check sorting of characters', () => {
-  const characters = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ];
-  const standard = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
-  //let result = sortCharacters(characters);
-  const result = sortCharacters();
-  expect(result).toEqual(standard);
-});
-*/
-
 import sortCharacters from '../js/app.js';
 
 test('good health checking', () => {
